b17065bb1f56568dbeba38c47803ef2d468bdfee
Andrei Miulescu <lusu777@gmail.com>
2020-02-28 15:43:19 +1100

    Better version

--- package.sh
+++ package.sh
@@ -8,19 +8,8 @@ usage() {
 
 }
 
-helm package --destination charts/ webservice
-helm package --destination charts/ launcher
-helm package --destination charts/ web
+helm package webservice
+helm package launcher
+helm package web
 
-helm servecm --storage local --chart-url https://raw.githubusercontent.com/tunnelhero/hero_charts/master --storage-local-rootdir ./ &
-
-sleep 1
-
-ps ax | grep c[h]artmuseum | awk '{print $1}' | xargs kill
-
-in_file=index-cache.yaml
-out_file=index.yaml
-
-# # cat $in_file | sed 's#http://127.0.0.1:8879#https://raw.githubusercontent.com/tunnelhero/hero_charts/master#g' >$out_file
-
-mv $in_file $out_file
+helm repo index ./ --url https://raw.githubusercontent.com/tunnelhero/hero_charts/master
